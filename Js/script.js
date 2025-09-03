756cb54c78417e6b311e10f985f21b17695bf00d
Rafael Da Silva <rfsrafaelsilva@gmail.com>
2025-09-03 16:22:02 -0300

    Add js na galeria imagens, add plugin animar na home e seguros, e add .js no html das pagimas para deixar sempre habilitado o js, mesmo se for javascript desabilitado

--- Js/script.js
+++ Js/script.js
@@ -26,20 +26,41 @@ parametros.forEach(ativarParamentro);
 // Ativar Perguntas frequentes
 
 const perguntas = document.querySelectorAll(".perguntas button");
-
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
-
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
   pergunta.setAttribute("aria-expanded", "true");
   console.log(resposta);
 }
-
 function eventoPerguntas(pergunta) {
   pergunta.addEventListener("click", ativarPergunta);
 }
-
 perguntas.forEach(eventoPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagen(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width:1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagen);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Ativar animações se script ativo no html
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
